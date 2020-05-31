b7b0581b7966a4501546b743787622f5fb31fed6
Juan Rivillas <jprivillaso@gmail.com>
2020-05-31 19:44:31 -0300

    Added control elements

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import * as S from './styled';
+import {
+  Horizontal
+} from '../../commons/styled';
 
 function Footer() {
   return (
     <S.Footer>
-      Footer
+      <Horizontal>
+        <S.SpeedControl>
+          <S.Text
+            class="active"
+          > 1X </S.Text>
+        </S.SpeedControl>
+        <S.SpeedControl>
+          <S.Text>1.5X</S.Text>
+        </S.SpeedControl>
+        <S.SpeedControl>
+          <S.Text>2X</S.Text>
+        </S.SpeedControl>
+      </Horizontal>
     </S.Footer>
   );
 }
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
+import {
+  mainColor
+} from '../../commons/colors';
+
 export const Footer = styled.footer`
   grid-area: footer;
+`;
+
+export const SpeedControl = styled.div`
+  width: 100px;
+  height: 50px;
+  border: solid 1px #888888;
+  font-size: 1em;
+  margin: 0 1em;
+`;
+
+export const Text = styled.p`
+  font-size: 1em;
+
+  &.active {
+    border: solid 1px ${mainColor}
+  }
 `;

fcd617cd1356d42650d354e10e53585c661cd1b7
Juan Rivillas <jprivillaso@gmail.com>
2020-05-31 19:33:11 -0300

    Implemented body elements

--- src/components/Body/styled.js
+++ src/components/Body/styled.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
+import { FiPauseCircle } from 'react-icons/fi';
+
+import {
+  errorColor
+} from '../../commons/colors';
 
 export const Body = styled.main`
   grid-area: body;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InfoText = styled.p`
+  color: ${errorColor}
+`;
+
+export const Time = styled.p`
+  font-size: 10em;
+  margin: 0;
+  margin-right: 0.2em;
+`;
+
+export const PauseIcon = styled(FiPauseCircle)`
+  font-size: 5em;
 `;

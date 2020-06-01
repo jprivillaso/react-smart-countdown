9efdcc5799ce355cae42290d18fb23a7a00d65d5
Juan Rivillas <jprivillaso@gmail.com>
2020-06-01 19:24:29 -0300

    Added comment

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,6 +1,7 @@
 import { useState, useCallback } from 'react';
 import { StatusOrValueContext } from '../state/context';
 
+// Use this hook in order to keep the status and value from the countdown
 export const useCountdown = (): StatusOrValueContext => {
   const [countdownStatus, setStatus] = useState(false);
   const [countdownValue, setValue] = useState('');

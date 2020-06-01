dfd8c274dfe3719a4d6e54a1545f8fa8911d2c0e
Juan Rivillas <jprivillaso@gmail.com>
2020-06-01 18:23:31 -0300

    Added initial state handling for the input change event

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as S from './styled';
+import { initialTime } from '../../state';
 
 function Header() {
+  const [time, setTime] = useState(initialTime);
+
+  const updateTime = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const value: string = event.currentTarget.value;
+    setTime(value);
+  }
+
   return (
     <S.Header>
       <S.Vertical>
         <S.Text>Smart Countdown</S.Text>
         <div>
           <S.Input
-          placeholder="02:30"
+            placeholder="02:30"
+            value={time || ''}
+            onChange={
+              (e: React.SyntheticEvent<HTMLInputElement>) => updateTime(e)
+            }
           ></S.Input>
-          <S.Button>Start</S.Button>
+          <S.Button
+            onClick={() => console.log('asd')}
+          >Start</S.Button>
         </div>
       </S.Vertical>
     </S.Header>
---
+++ src/state/index.ts
@@ -0,0 +1,4 @@
+export const initialTime: string = '';
+export const initialCountdown: boolean = false;
+
+export default {}

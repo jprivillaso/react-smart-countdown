19f642d4e6679adbb86e7ff6cada54bd26f59fac
Juan Rivillas <jprivillaso@gmail.com>
2020-06-02 00:10:59 -0300

    Finshed control speed adjustments and style amendments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,31 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import * as S from './styled';
 import {
   Horizontal
 } from '../../commons/styled';
-import CountdownContext from '../../state/context';
+import CountdownContext, { Status } from '../../state/context';
+import Swal from 'sweetalert2';
 
 function Footer() {
-  const { setCurrentSpeed } = useContext(CountdownContext);
+  const { setCurrentSpeed, countdownStatus } = useContext(CountdownContext);
+  const [ active, setActive ] = useState('1000');
+
+  const updateSpeedControl = (speed: number) => {
+    if (countdownStatus === Status.Started || countdownStatus === Status.HalfPassed) {
+      setCurrentSpeed(speed);
+      setActive(`${speed}`);
+    } else {
+      Swal.fire(
+        'Error',
+        'Countdown must be in progress in order to be able to change the speed',
+        'error'
+      );
+    }
+  };
 
   return (
     <S.Footer>
       <Horizontal>
-        <S.SpeedControl className="active">
-          <S.Text
-            onClick={() => setCurrentSpeed(1000)}
-          > 1X </S.Text>
+        <S.SpeedControl
+          className={active === '1000' ? 'active' : ''}
+          onClick={() => updateSpeedControl(1000)}
+        >
+          <S.Text> 1X </S.Text>
         </S.SpeedControl>
-        <S.SpeedControl>
-          <S.Text
-            onClick={() => setCurrentSpeed(800)}
-          >1.5X</S.Text>
+        <S.SpeedControl
+          className={active === '600' ? 'active' : ''}
+          onClick={() => updateSpeedControl(600)}
+        >
+          <S.Text>1.5X</S.Text>
         </S.SpeedControl>
-        <S.SpeedControl>
-          <S.Text
-            onClick={() => setCurrentSpeed(500)}
-          >2X</S.Text>
+        <S.SpeedControl
+          className={active === '400' ? 'active' : ''}
+          onClick={() => updateSpeedControl(400)}
+        >
+          <S.Text>2X</S.Text>
         </S.SpeedControl>
       </Horizontal>
     </S.Footer>
